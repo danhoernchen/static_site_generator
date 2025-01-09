4a3a197c3210179f629a108373f5ae8fdee80a0d
Daniel <191582347+danhoernchen@users.noreply.github.com>
2025-01-09 09:56:21 +0100

    added LeafNode, ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,3 +30,26 @@ class LeafNode(HTMLNode):
             return f'<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>'
         else:
             return f'<{self.tag}>{self.value}</{self.tag}>'
+        
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+        
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("Node must have a tag")
+        if self.children == None:
+            raise ValueError("Node must have children")
+        elif self.props != None:
+            children_string = self.__create_children_string(self.children)
+            return f'<{self.tag} {self.props_to_html()}> {children_string} </{self.tag}>'
+        else:
+            children_string = self.__create_children_string(self.children)
+            print(children_string)
+            return f'<{self.tag}> {children_string} </{self.tag}>'
+        
+    def __create_children_string(self,children):
+        children_string = ""
+        for child in children:
+                children_string = children_string + child.to_html()
+        return children_string
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 props = {
     "href": "https://www.google.com", 
@@ -33,6 +33,30 @@ class TestTextNode(unittest.TestCase):
     def test_leaf_txt(self):
         node = LeafNode(tag=None,value="just some raaaaaaw text")
         print(node.to_html())
+        
+    def test_parent_node(self):
+        child1 = LeafNode("a", "Clickity Linkity",{"href":"einhoernchen.org"})
+        child2 = LeafNode("p", "paragraph textity text")
+        parent_node = ParentNode("div",[child1, child2])
+        print(parent_node.to_html())
+        
+    def test_parent_node(self):
+        child1 = LeafNode("a", "Clickity Linkity",{"href":"einhoernchen.org"})
+        child2 = LeafNode("p", "paragraph textity text")
+        parent_node1 = ParentNode("div",[child1, child2])
+        child_3 = LeafNode(tag="img",value = "123", props={"src":"blablub.com/yepyep.png"})
+        parent_node2 = ParentNode("div",[child_3, parent_node1],props = {"class":"container"})
+        print(parent_node2.to_html())
+        
+    def test_parent_node(self):
+        child1 = LeafNode("a", "Clickity Linkity",{"href":"einhoernchen.org"})
+        child2 = LeafNode("p", "paragraph textity text")
+        parent_node1 = ParentNode("div",[child1, child2])
+        child_3 = LeafNode(tag="img",value = "123", props={"src":"blablub.com/yepyep.png"})
+        parent_node2 = ParentNode("div",[child_3, parent_node1],props = {"class":"container"})
+        child_4 = LeafNode("p","abcdef",{"class":"abc"})
+        parent_node3 = ParentNode("div", [parent_node1, parent_node2])
+        print(parent_node3.to_html())
     
         
 if __name__ == "__main__":
